f3c8ff59908d05a119e077dbf308544343c1f802
crndolo <ndoloraphael@gmail.com>
2019-05-07 11:06:01 +0300

    Global header and footer files for the front end are created.

---
+++ include/footer.php
@@ -0,0 +1,16 @@
+            <div class="row">
+                <div class="col-sm-12 site-footer">
+                    <footer id="colophon" role="contentinfo">
+                        <div class="site-info">
+                            Copyright &copy; <?php echo date('Y'); ?> Azury Traders. All Rights Reserved. <br/>
+                                Powered by Quinta <span class="sep"> | </span> Azury
+
+                        </div>
+                    </footer>
+                </div>
+            </div>            
+            
+        </div>
+        <!-- END CONTAINER -->
+    </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta content="" name="author" />
+        <meta content="" name="description" />        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">        
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+        <title> Azury Traders </title>
+
+        <!-- BEGIN CSS FILES -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" rel="stylesheet" />
+        <link href="assets/font-awesome/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
+        <link href="assets/font-awesome/css/font-awesome.css" type="text/css" rel="stylesheet" />
+        <link href="assets/css/login-4.css" rel="stylesheet" type="text/css" rel="stylesheet" />
+        <link href="assets/css/components.css" rel="stylesheet" type="text/css" rel="stylesheet" />
+        <link href="assets/css/components.min.css" rel="stylesheet" type="text/css" rel="stylesheet" />
+        <link href="assets/css/frontend.css" rel="stylesheet" type="text/css" rel="stylesheet" />
+        <!-- END CSS FILES -->
+        
+        <!-- START JAVASCRIPT FILES -->
+        <script type="text/javascript" src="assets/js/jquery.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.slideshow.min.js"></script>
+	
+        <script type="text/javascript">
+            $(document).ready(function() {
+                    $('.slideshow').slideshow({
+                            timeout: 5000,
+                            fadetime: 2000,
+                            type: 'sequence'
+                    });
+            });
+	</script>
+        <!-- END JAVASCRIPT FILES -->
+    </head>
+    
+    <body>
+        <!-- BEGIN CONTAINER -->
+        <div class="container-fluid">
+            <!-- BEGIN ROW -->
+            <div class="row social-media">
+                <div class="col-sm-6">                  
+                    
+                </div>
+                
+                <div class="col-sm-6">
+                    <ul class="nav navbar-social" >
+                        <li> <a href="http://www.facebook.com" target="_blank"> <i class="fa fa-facebook"> </i> </a> </li>
+                        <li> <a href="http://www.twitter.com" target="_blank"> <i class="fa fa-twitter"> </i>  </a> </li>
+                        <li> <a href="http://www.instagram.com" target="_blank"> <i class="fa fa-instagram"> </i>  </a> </li>
+                        <li> <a href="http://www.google.com" target="_blank"> <i class="fa fa-google-plus"> </i>  </a> </li>
+                        <li> <a href="http://www.pinterest.com" target="_blank"> <i class="fa fa-pinterest"> </i>  </a> </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- END ROW` -->
+            <!-- CLEAR ALL FLOAT -->
+            <div class="clear-float"></div>
+            <!-- CLEAR ALL FLOAT -->
+            
+            <!-- BEGIN ROW -->
+            <div class="row upper-bar">
+                <div class="col-sm-3">
+                    AZURY Traders
+                </div>
+                
+                <div class="col-sm-8">
+                    <form role="form" method="POST" id="search" name="search" action="">
+                    	<div class="row">
+                            <div class="col-md-10">
+                                <input type="text" id="keyword" name="keyword" placeholder="Enter Keyword" class="form-control">
+                            </div>
+                            <div class="col-md-2">
+                                <input type="submit" id="submit" name="submit" value="Search" class="form-control">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                
+                <div class="col-sm-1">
+                    <!-- SHOPPING CART LINK -->
+                    <a href="#">
+                        <i class="fa fa-shopping-cart fa-2x"></i>
+                    </a>
+                    <!-- SHOPPING CART LINK -->
+                </div>
+            </div>
+            <!-- END ROW -->
+            
+            <!-- BEGIN MENU ROW-->
+            <div class="row menu">
+                <div class="col-sm-12">
+                    <ul class="nav navbar-nav">
+                        <li> <a href="index.php"> Home </a> </li>
+                        <li> <a href=""> About us </a> </li>
+                        <li> <a href=""> Contact Us </a> </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- END MENU ROW-->
+            
+            
+            
+        
