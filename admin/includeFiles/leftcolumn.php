8e550fc13c0c5193a1df94ee49064bc5d9e7b7cd
crndolo <ndoloraphael@gmail.com>
2019-07-09 16:56:46 +0300

    Creating controls for admin levels in left column menu for Admin side

--- admin/includeFiles/leftcolumn.php
+++ admin/includeFiles/leftcolumn.php
@@ -36,38 +36,51 @@
     <!-- END DASHBOARD ROW -->
     
     <!-- BEGIN ADMINISTRATOR MENU ROW -->
+    <?php if($_SESSION['admin_add']=="Yes" AND $_SESSION['admin_view_all']=="Yes"){ ?>
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xl-12 menubox">
             <div class="menu-title  <?php echo $administrator; ?> "> Administrators </div>
             <ul>
                 <li>
-                    <a href="administratorAdd.php"> Add New </a>
+                    <?php if($_SESSION['admin_add']=="Yes"){ ?>
+                        <a href="administratorAdd.php"> Add New </a>
+                    <?php } ?>
                 </li>
                 <li>
-                    <a href="administratorViewAll.php"> View All </a>
+                    <?php if($_SESSION['admin_view_all']=="Yes"){ ?>
+                        <a href="administratorViewAll.php"> View All </a>
+                    <?php } ?>
                 </li>
             </ul>
         </div>
     </div>
+    <?php } ?>
     <!-- END ADMINISTRATOR MENU ROW -->
     
     <!-- BEGIN CUSTOMER MENU ROW -->
+    <?php if($_SESSION['category_add']=="Yes" AND $_SESSION['category_view_all']=="Yes"){ ?>
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xl-12 menubox">
             <div class="menu-title  <?php echo $category; ?> "> Category </div>
             <ul>
                 <li>
-                    <a href="categoryAdd.php"> Add New </a>
+                    <?php if($_SESSION['category_add']=="Yes"){ ?>
+                        <a href="categoryAdd.php"> Add New </a>
+                    <?php } ?>
                 </li>
                 <li>
-                    <a href="categoryViewAll.php"> View All </a>
+                    <?php if($_SESSION['category_view_all']=="Yes"){ ?>
+                        <a href="categoryViewAll.php"> View All </a>
+                    <?php } ?>
                 </li>
             </ul>
         </div>
     </div>
+    <?php } ?>
     <!-- END CUSTOMER MENU ROW -->
     
     <!-- BEGIN CUSTOMER MENU ROW -->
+    <?php if($_SESSION['customer_view_all']=="Yes"){ ?>
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xl-12 menubox">
             <div class="menu-title  <?php echo $customer; ?> "> Customer </div>
@@ -78,41 +91,55 @@
             </ul>
         </div>
     </div>
+    <?php } ?>
     <!-- END CUSTOMER MENU ROW -->
     
     <!-- BEGIN PRODUCT MENU ROW -->
+    <?php if($_SESSION['product_add']=="Yes" AND $_SESSION['product_view_all']=="Yes"){ ?>
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xl-12 menubox">
             <div class="menu-title  <?php echo $product; ?> "> Products </div>
             <ul>
                 <li>
-                    <a href="productAdd.php"> Add New </a>
+                    <?php if($_SESSION['product_add']=="Yes"){ ?>
+                        <a href="productAdd.php"> Add New </a>
+                    <?php } ?>
                 </li>
                 <li>
-                    <a href="productViewAll.php"> View All </a>
+                    <?php if($_SESSION['product_view_all']=="Yes"){ ?>
+                        <a href="productViewAll.php"> View All </a>
+                    <?php } ?>
                 </li>
             </ul>
         </div>
     </div>
+    <?php } ?>
     <!-- END PRODUCT MENU ROW -->
     
     <!-- BEGIN SLIDE SHOW MENU ROW -->
+    <?php if($_SESSION['slideshow_add']=="Yes" AND $_SESSION['slideshow_view_all']=="Yes"){ ?>
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xl-12 menubox">
             <div class="menu-title  <?php echo $slideShow; ?> "> Slide Show </div>
             <ul>
                 <li>
-                    <a href="slideShowAdd.php"> Add New </a>
+                    <?php if($_SESSION['slideshow_add']=="Yes"){ ?>
+                        <a href="slideShowAdd.php"> Add New </a>
+                    <?php } ?>
                 </li>
                 <li>
-                    <a href="slideShowViewAll.php"> View All </a>
+                    <?php if($_SESSION['slideshow_view_all']=="Yes"){ ?>
+                        <a href="slideShowViewAll.php"> View All </a>
+                    <?php } ?>
                 </li>
             </ul>
         </div>
     </div>
+    <?php } ?>
     <!-- END SLIDE SHOW MENU ROW -->
     
     <!-- BEGIN STOCK MENU ROW -->
+    <?php if($_SESSION['stock_add']=="Yes"){ ?>
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xl-12 menubox">
             <div class="menu-title  <?php echo $stock; ?> "> Stocks </div>
@@ -123,10 +150,11 @@
             </ul>
         </div>
     </div>
+    <?php } ?>
     <!-- END STOCK MENU ROW -->
     
     <!-- BEGIN ORDER MENU ROW -->
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-md-12 col-sm-12 col-xl-12 menubox">
             <div class="menu-title  <?php echo $order; ?> "> Orders </div>
             <ul>                
@@ -135,7 +163,7 @@
                 </li>
             </ul>
         </div>
-    </div>
+    </div> -->
     <!-- END ORDER MENU ROW -->
 
     <!-- BEGIN ORDER MENU ROW -->
