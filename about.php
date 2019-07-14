91b3c4d18c86e4a26a37f360270a6a9466f1d59f
crndolo <ndoloraphael@gmail.com>
2019-07-15 00:38:38 +0300

    Recreating home page

--- about.php
+++ about.php
@@ -31,68 +31,22 @@
     }
 ?>
 <!-- END SLIDE SHOW ROW-->
-
-<!-- BEGIN PRODUCT ADVERTISEMENT SECTION -->
-<div class="row category-row">    
-    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-        <div class="category-identifier">
-            <img src="assets/img/jackets.jpg">
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-        <div class="category-identifier">
-            <img src="assets/img/discount_jacket.jpg">
-            <img src="assets/img/bestjacket.jpg">
-        </div>
-    </div>
-
-    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-        <div class="category-identifier">
-            <img src="assets/img/winter.jpg">
-            <img src="assets/img/fitting.jpg">
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 advert">
-        <img src="assets/img/advert.jpg">
-    </div>
-</div>
-
-<div class="row category-row">    
-    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-        <div class="category-identifier">
-            <img src="assets/img/shoes.jpg">
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-        <div class="category-identifier">
-            <img src="assets/img/leather.jpg">
-            <img src="assets/img/sport.jpg">
-        </div>
-    </div>
-
-    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-        <div class="category-identifier">
-            <img src="assets/img/warm.jpg">
-            <img src="assets/img/loved.jpg">
-        </div>
-    </div>
-</div>
-<!-- END PRODUCT ADVERTISEMENT SECTION -->
-
-<!--        
+        
 <?php
     while($category_row = mysqli_fetch_assoc($category_result)){
         $categoryId = $category_row['categoryId'];
         
+        /*$product_query = "SELECT * FROM product WHERE categoryId = '$categoryId' ORDER BY productName ASC LIMIT 4";
+        $product_result = mysqli_query($connect, $product_query);
+        $product_num_rows = mysqli_num_rows($product_result);
+        
+        if($product_num_rows){*/
             ?>
--->
+
             <!-- BEGIN ROW -->
-            <!--<div class="row">-->
+            <div class="row">
                 <!-- BEGIN CATEGORY TITLE -->
-                <!--<div class="col-sm-12 product-title-bar">
+                <div class="col-sm-12 product-title-bar">
                     <div class="title">
                         <?php echo $category_row['categoryName']; ?>
                     </div>  
@@ -100,19 +54,19 @@
                     <div class="next">
                         <a href="category.php?id=<?php echo $categoryId; ?>"> See all  <i class="fa fa-chevron-right"> </i> </a>
                     </div>
-                </div>-->
+                </div>
                 <!-- END CATEGORY TITLE -->
-            <!--</div> -->
+            </div>
             <!-- END ROW -->
             
             <!-- BEGIN ROW -->
-            <!--<div class="row"> 
+            <div class="row"> 
             
             <?php            
                 ?>
-                -->
+                
                 <!-- BEGIN CATEGORY ITEMS -->
-                <!--<div class="col-md-3">
+                <div class="col-md-3">
                     <div class="product-item">
                         <a href="category.php?id=<?php echo $category_row['categoryId']; ?>">
                             <div class="product-image">
@@ -122,24 +76,24 @@
                     </div>
                 </div>
                 <!-- END CATEGORY ITEMS -->
-                <!--<div class="row">
+                <div class="row">
                     <div class="col-md-8">
                         <?php echo $category_row['description'] ; ?>
                         <!--<div class="product-name">
                             <a href="product.php?id=<?php echo $product_row['productId']; ?>"> <?php echo $product_row['productName'] ; ?> </a>
                         </div> -->
-                    <!--</div>
-                </div>-->
-                <!--<?php
+                    </div>
+                </div>
+                <?php
             //}
             ?>
-            </div>-->
+            </div>
             <!-- END ROW -->
-            <!--<?php
+            <?php
             
         //}
     }
-?>                 -->           
+?>                            
         
 <?php
     include('include/footer.php');
